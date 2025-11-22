2eb94ded1c2f486bafce974ee51834e0d86c7127
shivam kumar <shivamwt59@gmail.com>
2025-11-22 12:06:28 +0530

    Add Day 1 Python practice

---
+++ codes/day1.py
@@ -0,0 +1,18 @@
+# Day 1 - Python Basic
+
+# Taking name and Age as input
+name = input("Enter your Name : ")
+age = int(input("Enter your age : "))
+
+# Printing Message
+print("\nHello", name)
+print("your are",age ,"years old")
+
+# Datatypes Examples
+x = 23
+y = 34.4
+z = "python"
+is_coding = True
+
+print("\nData types:")
+print(type(x), type(y), type(z), type(is_coding))
